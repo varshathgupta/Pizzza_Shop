641e4a07049a3b98ffe5154f1a17c20c9af06248
varshathgupta <varshathgupta@gmail.com>
2022-03-10 17:47:04 +0530

    Pizzzzza Done

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useCart } from "react-use-cart";
 import styled from 'styled-components';
-import "bootstrap/dist/css/bootstrap.min.css";
+
 
 
 export const Empty = styled.div`
